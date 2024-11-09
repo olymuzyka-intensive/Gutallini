4542410f4e5c55218801a433bc93e9d91b1604ec
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-11-10 02:30:57 +0300

    add new style

--- src/components/TelegramForm/TelegramForm.jsx
+++ src/components/TelegramForm/TelegramForm.jsx
@@ -12,7 +12,7 @@ export const TelegramForm = ({ isOpen, onClose }) => {
   const [textArea, setTextArea] = useState("");
   const [selectedOption, setSelectedOption] = useState("");
 
-  const [textMessage, setTextMessage] = useState(false)
+  const [textMessage, setTextMessage] = useState(false);
 
   const handleFileChange = (event) => {
     setPhotos([...event.target.files]);
@@ -37,7 +37,7 @@ export const TelegramForm = ({ isOpen, onClose }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    setTextMessage(true)
+    setTextMessage(true);
 
     const token = "7729156275:AAE2Nd1uYtkddV8W_bOtpZogGyEh_yfShT0";
     const chatId = "1010490009";
@@ -51,9 +51,8 @@ export const TelegramForm = ({ isOpen, onClose }) => {
       body: JSON.stringify({
         chat_id: chatId,
         text: `Имя: ${textInputName}\nТелефон: ${textInputPhone}\nУслуга: ${selectedOption}\nКомментарий: ${textArea}`,
-      })
-      
-    })   
+      }),
+    });
 
     const data = new FormData();
     photos.forEach((photo) => {
@@ -76,136 +75,137 @@ export const TelegramForm = ({ isOpen, onClose }) => {
 
     setTimeout(() => {
       setTextMessage(false);
-      onClose()
+      onClose();
     }, 5000);
-    
   };
-
 
   return (
     <>
-    <form className="form" ref={form} onSubmit={handleSubmit} id="form">
-      <div onClick={onClose} className="form__close">
-        <svg
-          width="30px"
-          height="30px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+      <form className="form" ref={form} onSubmit={handleSubmit} id="form">
+        <div onClick={onClose} className="form__close">
+          <svg
+            width="30px"
+            height="30px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0" />
 
-          <g
-            id="SVGRepo_tracerCarrier"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-
-          <g id="SVGRepo_iconCarrier">
-            <path
-              d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z"
-              stroke="#d78707"
-              strokeWidth="1.5"
-            />
-            <path
-              d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5"
-              stroke="#d78707"
-              strokeWidth="1.5"
+            <g
+              id="SVGRepo_tracerCarrier"
               strokeLinecap="round"
+              strokeLinejoin="round"
             />
-          </g>
-        </svg>
-      </div>
-      <p className="form__title">Отправить фото</p>
-      <input
-        type="name"
-        name="name"
-        id="name"
-        maxLength="50"
-        placeholder="Ваше имя"
-        required
-        value={textInputName}
-        onChange={handleInputChangeName}
-      />
-      <input
-        type="tel"
-        name="tel"
-        id="tel"
-        placeholder="Ваш номер телефона"
-        pattern="^\+375\s?\(?(29|25|44|33|17)\)?\s?\d{3}-?\d{2}-?\d{2}$"
-        required
-        value={textInputPhone}
-        onChange={handleInputChangePhone}
-      />
-      <label className="form__subtitle" htmlFor="option">
-        Какая услуга Вам необходима?
-      </label>
-      <p className="form__description">
-        Выберите вариант из представленного списка
-      </p>
-      <select
-        name="option-services"
-        id="option"
-        value={selectedOption}
-        onChange={handleSelectChange}
-        required
-      >
-        <option value="" disabled> </option>
-        <option value="Полный уход с обновлением цвета">
-          Полный уход с обновлением цвета
-        </option>
-        <option value="Покраска подошвы">Покраска подошвы</option>
-        <option value="Замша. Покраска в родной цвет">
-          Замша. Покраска в родной цвет
-        </option>
-        <option value="Чистка и обновление обуви из замши">
-          Чистка и обновление обуви из замши
-        </option>
-        <option value="Чистка и обновление обуви из кожи">
-          Чистка и обновление обуви из кожи
-        </option>
-        <option value="Реставрация, устранение порезов, царапин, трещин">
-          Реставрация, устранение порезов, царапин, трещин
-        </option>
-        <option value="Текстиль. Полный уход">Текстиль. Полный уход</option>
-        <option value="Нубук. Полный уход">Нубук. Полный уход</option>
-        <option value="Выведение реагентов">Выведение реагентов</option>
-        <option value="Восстановление сумок">Восстановление сумок</option>
-        <option value="Полный уход комбинированные материалы">
-          Полный уход комбинированные материалы
-        </option>
-        <option value="Патинирование">Патинирование</option>
-      </select>
-      <label className="form__subtitle" htmlFor="comments">
-        Комментарии
-      </label>
-      <textarea
-        value={textArea}
-        onChange={handleTextAreaChange}
-        name="comments"
-        id="comments"
-        placeholder="Опишите пожалуйста вашу проблему"
-      ></textarea>
-      <label className="form__subtitle" htmlFor="files">
-        Загрузить фото
-      </label>
-      <input
-        type="file"
-        id="files"
-        accept="image/*"
-        multiple
-        required
-        onChange={handleFileChange}
-      />
 
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z"
+                stroke="#d78707"
+                strokeWidth="1.5"
+              />
+              <path
+                d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5"
+                stroke="#d78707"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              />
+            </g>
+          </svg>
+        </div>
+        <p className="form__title">Отправить фото</p>
+        <input
+          type="name"
+          name="name"
+          id="name"
+          maxLength="50"
+          placeholder="Ваше имя"
+          required
+          value={textInputName}
+          onChange={handleInputChangeName}
+        />
+        <input
+          type="tel"
+          name="tel"
+          id="tel"
+          placeholder="Ваш номер телефона"
+          pattern="^\+375\s?\(?(29|25|44|33|17)\)?\s?\d{3}-?\d{2}-?\d{2}$"
+          required
+          value={textInputPhone}
+          onChange={handleInputChangePhone}
+        />
+        <label className="form__subtitle" htmlFor="option">
+          Какая услуга Вам необходима?
+        </label>
+        <p className="form__description">
+          Выберите вариант из представленного списка
+        </p>
+        <select
+          name="option-services"
+          id="option"
+          value={selectedOption}
+          onChange={handleSelectChange}
+          required
+        >
+          <option value="" disabled>
+          </option>
+          <option value="Полный уход с обновлением цвета">
+            Полный уход с обновлением цвета
+          </option>
+          <option value="Покраска подошвы">Покраска подошвы</option>
+          <option value="Замша. Покраска в родной цвет">
+            Замша. Покраска в родной цвет
+          </option>
+          <option value="Чистка и обновление обуви из замши">
+            Чистка и обновление обуви из замши
+          </option>
+          <option value="Чистка и обновление обуви из кожи">
+            Чистка и обновление обуви из кожи
+          </option>
+          <option value="Реставрация, устранение порезов, царапин, трещин">
+            Реставрация, устранение порезов, царапин, трещин
+          </option>
+          <option value="Текстиль. Полный уход">Текстиль. Полный уход</option>
+          <option value="Нубук. Полный уход">Нубук. Полный уход</option>
+          <option value="Выведение реагентов">Выведение реагентов</option>
+          <option value="Восстановление сумок">Восстановление сумок</option>
+          <option value="Полный уход комбинированные материалы">
+            Полный уход комбинированные материалы
+          </option>
+          <option value="Патинирование">Патинирование</option>
+        </select>
+        <label className="form__subtitle" htmlFor="comments">
+          Комментарии
+        </label>
+        <textarea
+          value={textArea}
+          onChange={handleTextAreaChange}
+          name="comments"
+          id="comments"
+          placeholder="Опишите пожалуйста вашу проблему"
+        ></textarea>
+        <div className="form__upload">
+          <div className="form__subtitle" htmlFor="files">Загрузить фото</div>
+          <input
+            type="file"
+            className="form__upload-file"
+            id="files"
+            accept="image/*"
+            multiple
+            required
+            onChange={handleFileChange}
+          />
+          {/* <label className="form__upload-file" htmlFor="files"> Прикрепить файлы
+          <img src="/public/img/icons/upload.svg" alt="upload" /> 
+          <span class="form__upload-text">Прикрепить файлы</span>
+          </label> */}
+        </div>
 
-      <button className="btn btn--send_foto" type="submit">
-        Отправить
-      </button>
-      {textMessage && <div className="form__message">Заявка принята</div>}
-
-    </form>
-
-</>
+        <button className="btn btn--send_foto" type="submit">
+          Отправить
+        </button>
+        {textMessage && <div className="form__message">Заявка принята</div>}
+      </form>
+    </>
   );
 };
